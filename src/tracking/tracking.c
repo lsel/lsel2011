5a8bab808df6b15a7e1b1d5724c75e6ae8a99bbf
lsel <lsel@lsel01.(none)>
2011-04-12 12:09:57 +0200

    Tren de vapor <-> Tren diesel
    Al final el tren alto es el tren diesel

--- src/tracking/tracking.c
+++ src/tracking/tracking.c
@@ -10,7 +10,7 @@
 #define S3ABAJO 0x40 
 #define S3ARRIBA 0x80
 
-#define DELAY 200000
+#define DELAY 1000000
 #define DISTS0 3
 #define DISTS1 2
 #define DISTS2 1
@@ -38,7 +38,7 @@ struct control_tiempo {
 
 	FILE *driver_llegada;
   struct datos_sensores sensores, sensores_prev;
-	struct control_tiempo tiempoVapor, tiempoDiesel;
+	struct control_tiempo tiempodiesel, tiempovapor;
 
 int actualizarSensores(void){
 		sensores_prev=sensores;
@@ -52,81 +52,81 @@ int actualizarSensores(void){
 
 void estimacion (int tren, int zona){
 
-	//Estimo si es diesel. Valores por defecto para la primera vuelta
-		if (tiempoDiesel.t1 != 0){
-			tiempoDiesel.estimacionT0 = (DIST01*(tiempoDiesel.t1 - tiempoDiesel.t0))/(1000*DISTS0); //calcula la estimacion desde S0 en s.
-			tiempoDiesel.estimacionT1 = (DIST01*(tiempoDiesel.t1 - tiempoDiesel.t0))/(1000*DISTS1); //calcula la estimacion desde S1 en s.
-		}else{
-			tiempoDiesel.estimacionT0 = 14;
-			tiempoDiesel.estimacionT1 = 8;
-		}
-		if (tiempoDiesel.t2 !=0){
-			tiempoDiesel.estimacionT2 = (DIST12*(tiempoDiesel.t2 - tiempoDiesel.t1))/(1000*DISTS2); //calcula la estimacion desde S2 en s.
-		}else{
-			tiempoDiesel.estimacionT2 = 4;
-		}
-
 	//Estimo si es vapor. Valores por defecto para la primera vuelta
-		if (tiempoVapor.t1 != 0){
-			tiempoVapor.estimacionT0 = (DIST01*(tiempoVapor.t1 - tiempoVapor.t0))/(1000*DISTS0); //calcula la estimacion desde S0 en s.
-			tiempoVapor.estimacionT1 = (DIST01*(tiempoVapor.t1 - tiempoVapor.t0))/(1000*DISTS1); //calcula la estimacion desde S1 en s.
+		if (tiempovapor.t1 != 0){
+			tiempovapor.estimacionT0 = (DIST01*(tiempovapor.t1 - tiempovapor.t0))/(1000*DISTS0); //calcula la estimacion desde S0 en s.
+			tiempovapor.estimacionT1 = (DIST01*(tiempovapor.t1 - tiempovapor.t0))/(1000*DISTS1); //calcula la estimacion desde S1 en s.
 		}else{
-			tiempoVapor.estimacionT0 = 14;
-			tiempoVapor.estimacionT1 = 8;
+			tiempovapor.estimacionT0 = 14;
+			tiempovapor.estimacionT1 = 8;
 		}
-		if (tiempoVapor.t2 !=0){
-			tiempoVapor.estimacionT2 = (DIST12*(tiempoVapor.t2 - tiempoVapor.t1))/(1000*DISTS2); //calcula la estimacion desde S2 en s.
+		if (tiempovapor.t2 !=0){
+			tiempovapor.estimacionT2 = (DIST12*(tiempovapor.t2 - tiempovapor.t1))/(1000*DISTS2); //calcula la estimacion desde S2 en s.
 		}else{
-			tiempoVapor.estimacionT2 = 4;
+			tiempovapor.estimacionT2 = 4;
 		}
 
-	if (tren == 0){  // vapor
-		if (zona == 0){
-			printf("(estimacion) Tren de vapor saliendo de la estacion\n");
-			printf("(estimacion) Tiempo estimado de llegada para el tren de vapor: %ld segundos. \n", tiempoVapor.estimacionT0);
+	//Estimo si es diesel. Valores por defecto para la primera vuelta
+		if (tiempodiesel.t1 != 0){
+			tiempodiesel.estimacionT0 = (DIST01*(tiempodiesel.t1 - tiempodiesel.t0))/(1000*DISTS0); //calcula la estimacion desde S0 en s.
+			tiempodiesel.estimacionT1 = (DIST01*(tiempodiesel.t1 - tiempodiesel.t0))/(1000*DISTS1); //calcula la estimacion desde S1 en s.
+		}else{
+			tiempodiesel.estimacionT0 = 14;
+			tiempodiesel.estimacionT1 = 8;
 		}
-		if (zona == 1){
-			printf("(estimacion) Tiempo estimado de llegada para el tren de vapor: %ld segundos. \n", tiempoVapor.estimacionT1);
+		if (tiempodiesel.t2 !=0){
+			tiempodiesel.estimacionT2 = (DIST12*(tiempodiesel.t2 - tiempodiesel.t1))/(1000*DISTS2); //calcula la estimacion desde S2 en s.
+		}else{
+			tiempodiesel.estimacionT2 = 4;
 		}
-		if (zona == 2){
-			printf("(estimacion) Tiempo estimado de llegada para el tren de vapor: %ld segundos. \n", tiempoVapor.estimacionT2);
-		}
-		if (zona == 3){
-			printf("(estimacion) Tren de vapor entrando en la estacion\n");
-			printf("(estimacion) Tiempo estimado de llegada para el tren de vapor: 0 segundos. \n");
-		}
-	}
 
-	if (tren == 1){  // diesel
+	if (tren == 0){  // diesel
 		if (zona == 0){
-			printf("(estimacion) Tren de vapor saliendo de la estacion\n");
-			printf("(estimacion) Tiempo estimado de llegada para el tren diesel: %ld segundos. \n", tiempoDiesel.estimacionT0);
+			printf("(estimacion) Tren diesel saliendo de la estacion\n");
+			printf("(estimacion) Tiempo estimado de llegada para el tren diesel: %ld segundos. \n", tiempodiesel.estimacionT0);
 		}
 		if (zona == 1){
-			printf("(estimacion) Tiempo estimado de llegada para el tren diesel: %ld segundos. \n", tiempoDiesel.estimacionT1);
+			printf("(estimacion) Tiempo estimado de llegada para el tren diesel: %ld segundos. \n", tiempodiesel.estimacionT1);
 		}
 		if (zona == 2){
-			printf("(estimacion) Tiempo estimado de llegada para el tren diesel: %ld segundos. \n", tiempoDiesel.estimacionT2);
+			printf("(estimacion) Tiempo estimado de llegada para el tren diesel: %ld segundos. \n", tiempodiesel.estimacionT2);
 		}
 		if (zona == 3){
 			printf("(estimacion) Tren diesel entrando en la estacion\n");
 			printf("(estimacion) Tiempo estimado de llegada para el tren diesel: 0 segundos. \n");
 		}
 	}
+
+	if (tren == 1){  // vapor
+		if (zona == 0){
+			printf("(estimacion) Tren vapor saliendo de la estacion\n");
+			printf("(estimacion) Tiempo estimado de llegada para el tren de vapor: %ld segundos. \n", tiempovapor.estimacionT0);
+		}
+		if (zona == 1){
+			printf("(estimacion) Tiempo estimado de llegada para el tren de vapor: %ld segundos. \n", tiempovapor.estimacionT1);
+		}
+		if (zona == 2){
+			printf("(estimacion) Tiempo estimado de llegada para el tren de vapor: %ld segundos. \n", tiempovapor.estimacionT2);
+		}
+		if (zona == 3){
+			printf("(estimacion) Tren de vapor entrando en la estacion\n");
+			printf("(estimacion) Tiempo estimado de llegada para el tren de vapor: 0 segundos. \n");
+		}
+	}
 }
 
 int main(void){
     
-  int pos_tren_vapor=0, pos_tren_diesel=0; // de 0 a 3
+  int pos_tren_diesel=0, pos_tren_vapor=0; // de 0 a 3
 
   sensores_prev.estado = 0;
 
-	tiempoVapor.t0 = 0;
-	tiempoVapor.t1 = 0;
-	tiempoVapor.t2 = 0;
-	tiempoDiesel.t0 = 0;
-	tiempoDiesel.t1 = 0;
-	tiempoDiesel.t2 = 0;
+	tiempodiesel.t0 = 0;
+	tiempodiesel.t1 = 0;
+	tiempodiesel.t2 = 0;
+	tiempovapor.t0 = 0;
+	tiempovapor.t1 = 0;
+	tiempovapor.t2 = 0;
 
   driver_llegada = fopen("/var/sensores", "r");
   if(driver_llegada == NULL) {
@@ -148,14 +148,14 @@ int main(void){
 					usleep (DELAY);
 					if (actualizarSensores() ==0){
 						if ((sensores.estado & S0ARRIBA) != 0){
-							pos_tren_vapor = 0;
-							printf("(tracking) Tren de vapor entra en la zona 0\n");
-							tiempoVapor.t0 = hora;
-							estimacion(0,0);
-						}else {
 							pos_tren_diesel = 0;
 							printf("(tracking) Tren diesel entra en la zona 0\n");
-							tiempoDiesel.t0 = hora;
+							tiempodiesel.t0 = hora;
+							estimacion(0,0);
+						}else {
+							pos_tren_vapor = 0;
+							printf("(tracking) Tren de vapor entra en la zona 0\n");
+							tiempovapor.t0 = hora;
 							estimacion(1,0);
 						}
 					}
@@ -165,14 +165,14 @@ int main(void){
 					usleep (DELAY);
 					if (actualizarSensores() ==0){
 						if ((sensores.estado & S1ARRIBA) != 0){
-							pos_tren_vapor = 1;
-							printf("(tracking) Tren de vapor entra en la zona 1\n");
-							tiempoVapor.t1 = hora;
-							estimacion(0,1);
-						}else {
 							pos_tren_diesel = 1;
 							printf("(tracking) Tren diesel entra en la zona 1\n");
-							tiempoDiesel.t1 = hora;
+							tiempodiesel.t1 = hora;
+							estimacion(0,1);
+						}else {
+							pos_tren_vapor = 1;
+							printf("(tracking) Tren de vapor entra en la zona 1\n");
+							tiempovapor.t1 = hora;
 							estimacion(1,1);
 						}
 					}
@@ -182,14 +182,14 @@ int main(void){
 					usleep (DELAY);
 					if (actualizarSensores() ==0){
 						if ((sensores.estado & S2ARRIBA) != 0){
-							pos_tren_vapor = 2;
-							printf("(tracking) Tren de vapor entra en la zona 2\n");
-							tiempoVapor.t2 = hora;
-							estimacion(0,2);
-						}else {
 							pos_tren_diesel = 2;
 							printf("(tracking) Tren diesel entra en la zona 2\n");
-							tiempoDiesel.t2 = hora;
+							tiempodiesel.t2 = hora;
+							estimacion(0,2);
+						}else {
+							pos_tren_vapor = 2;
+							printf("(tracking) Tren de vapor entra en la zona 2\n");
+							tiempovapor.t2 = hora;
 							estimacion(1,2);
 						}
 					}
@@ -199,12 +199,12 @@ int main(void){
 					usleep (DELAY);
 					if (actualizarSensores() ==0){
 						if ((sensores.estado & S3ARRIBA) != 0){
-							pos_tren_vapor = 3;
-							printf("(tracking) Tren de vapor entra en la zona 3\n");
-							estimacion(0,3);
-						}else {
 							pos_tren_diesel = 3;
 							printf("(tracking) Tren diesel entra en la zona 3\n");
+							estimacion(0,3);
+						}else {
+							pos_tren_vapor = 3;
+							printf("(tracking) Tren de vapor entra en la zona 3\n");
 							estimacion(1,3);
 						}
 					}
